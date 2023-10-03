5f276e59e26f26f216daf53182d9c08fb0a2b63c
unknown <georgesroberto21@gmail.com>
2023-10-03 22:57:08 +0300

    Implemented Search form

--- Inventory/views.py
+++ Inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import ComputerForm
+from .models import Computer
+from .forms import ComputerForm, SearchForm
 
 # Create your views here.
 
@@ -10,26 +11,42 @@ def home(request):
     return render(request, 'home.html/', context)
 
 def list(request):
-    title = "View Computers"
+    title = "List of Computers"
     form = ComputerForm
+    queryset = Computer.objects.all()
+
+    search = SearchForm
+
+    context = { 
+        "title" : title,
+        "form" : form,
+        "queryset" : queryset,
+        "search" : search
+    }
+
+    
+
+    return render(request, 'list.html/', context)
+
+def add(request):
+    title = "Add Computers"
+    form = SearchForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list')
+
     context = { 
         "title" : title,
         "form" : form
     }
 
-    #if form.is_valid():
-    #    form.save()
-    #    return redirect('/')
-    
-    #context = { "title" : title, "form" : form }
 
 
-    return render(request, 'list.html/', context)
-
-def add(request):
-    return render(request, 'add.html/')
+    return render(request, 'add.html/', context)
 
 def update(request):
+
     return render(request, 'update.html/')
 
 def delete(request):
