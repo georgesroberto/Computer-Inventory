1385f08d506f1194cd97351ae3b0bcb44c6b3e45
unknown <georgesroberto21@gmail.com>
2023-10-03 22:58:24 +0300

    Introduced search formss

--- Inventory/forms.py
+++ Inventory/forms.py
@@ -4,6 +4,17 @@ from .models import Computer
 class ComputerForm(forms.ModelForm):
     class Meta:
         model = Computer
-        fields = ['pc_name','mac_address','ip_address','unit','location','added_by','created_at','updated_by','updated_at']
+        fields = ['pc_name','mac_address','ip_address','os','username','location']
 
-    
+class SearchForm(forms.ModelForm):
+    class Meta:
+        model = Computer
+        fields = ['pc_name', 'username']
+
+    def clean_item_name(self):
+        item_name = self.cleaned_data.get('pc_name')
+
+        if Computer.objects.filter(item_name=item_name).exists():
+            raise forms.ValidationError('An item with this name already exists.')
+
+        return item_name     
