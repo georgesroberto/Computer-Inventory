17b4ef3015274edd85cc0a9fc306b6cf7877a218
unknown <georgesroberto21@gmail.com>
2023-10-04 19:42:18 +0300

    Added date choice field...Started csv data export

--- Inventory/models.py
+++ Inventory/models.py
@@ -8,6 +8,8 @@ class Computer(models.Model):
     os = models.CharField(max_length=50, blank=True, null=True)
     username = models.CharField(max_length=50, blank=True, null=True)
     location = models.CharField(max_length=50, blank=True, null=True)
+    purchase_date = models.DateField(("Purchase Date | mm-dd-yy"), auto_now_add=False, auto_now=False, blank=True, null=True)
+    csv_export = models.BooleanField(blank=True, null=True)
     
     def __str__(self):
         return self.pc_name
