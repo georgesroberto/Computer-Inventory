d4f342074099845348221f36c8e501395f4e38b5
unknown <georgesroberto21@gmail.com>
2023-10-04 19:42:50 +0300

    Started csv data export

--- Inventory/forms.py
+++ Inventory/forms.py
@@ -4,17 +4,10 @@ from .models import Computer
 class ComputerForm(forms.ModelForm):
     class Meta:
         model = Computer
-        fields = ['pc_name','mac_address','ip_address','os','username','location']
+        fields = ['pc_name','mac_address','ip_address','os','username','location', 'purchase_date']
 
 class SearchForm(forms.ModelForm):
+    #csv_export = forms.BooleanField(required = False)
     class Meta:
         model = Computer
-        fields = ['pc_name', 'username']
-
-    def clean_item_name(self):
-        item_name = self.cleaned_data.get('pc_name')
-
-        if Computer.objects.filter(item_name=item_name).exists():
-            raise forms.ValidationError('An item with this name already exists.')
-
-        return item_name     
+        fields = ['pc_name', 'username']# 'csv_export']
