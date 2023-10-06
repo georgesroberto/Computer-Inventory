49929a02af8ef0200a01e5583fce0c21b2ebb83b
unknown <georgesroberto21@gmail.com>
2023-10-06 16:27:18 +0300

    Updated Models

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, date
 
 class Os_Choice(models.Model):
     os_system = models.CharField(max_length=60, blank=True, null=True)
@@ -8,14 +9,15 @@ class Os_Choice(models.Model):
 
 # Create your models here.
 class Computer(models.Model):
-    pc_name = models.CharField(max_length=50, blank=True, null=True) 
-    mac_address = models.CharField(max_length=50, blank=True, null=True) 
-    ip_address = models.CharField(max_length=50, blank=True, null=True)
-    os = models.ManyToManyField(Os_Choice, blank=True, null=True)
-    username = models.CharField(max_length=50, blank=True, null=True)
-    location = models.CharField(max_length=50, blank=True, null=True)
-    purchase_date = models.DateField(("Purchase Date | mm-dd-yy"), auto_now_add=False, auto_now=False, blank=True, null=True)
-    csv_export = models.BooleanField(blank=True, null=True)
+    pc_name = models.CharField(max_length=50, blank=False, null=True) 
+    mac_address = models.CharField(max_length=50, blank=False, null=True) 
+    ip_address = models.CharField(max_length=50, blank=False, null=True)
+    os = models.ManyToManyField(Os_Choice, blank=False)
+    username = models.CharField(max_length=50, blank=False, null=True)
+    location = models.CharField(max_length=50, blank=False, null=True)
+    update_date = models.DateField("Update Date (mm/dd/yyyy)",auto_now_add=False, auto_now=False, blank=True, null=True)
+    purchase_date = models.DateField()
+    csv_export = models.BooleanField(blank=False, null=True)
     
     def __str__(self):
         return self.pc_name
